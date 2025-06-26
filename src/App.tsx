ebf3502cc2a393156abe71ec10db0b18ea07ffcd
Developer <developer@company.com>
2025-06-26 15:06:00 +0530

    refactor: app changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import LoginPage from "./features/loginPage/loginPage";
-import { EmployeeDashboard } from "./features/employee";
-
 function App() {
-  // For demo purposes, you can toggle between login and dashboard
-  // In a real app, this would be controlled by authentication state
-  const showDashboard = true; // Change to false to show login page
 
   return <div>
-    {showDashboard ? <EmployeeDashboard /> : <LoginPage />}
+    <LoginPage />
   </div>;
 }
 
